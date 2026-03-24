3b10433afe2aa022dd1186be44074de287816590
2251010012chau-png <2251010012chau@ou.edu.vn>
2026-03-24 21:28:56 +0700

    add TaiKhoanDTO.java

---
+++ backend/src/main/java/group6/it/ou/sportfacilitybooking/dto/TaiKhoanDTO.java
@@ -0,0 +1,61 @@
+package group6.it.ou.sportfacilitybooking.dto;
+
+public class TaiKhoanDTO {
+
+    private Integer maTaiKhoan;
+    private String tenTaiKhoan;
+    private String matKhau;
+    private String role;           
+    private Integer maNhanVien;   
+    private Integer maKH;       
+
+// Getters & Setters
+
+    public Integer getMaTaiKhoan() {
+        return maTaiKhoan;
+    }
+
+    public void setMaTaiKhoan(Integer maTaiKhoan) {
+        this.maTaiKhoan = maTaiKhoan;
+    }
+
+    public String getTenTaiKhoan() {
+        return tenTaiKhoan;
+    }
+
+    public void setTenTaiKhoan(String tenTaiKhoan) {
+        this.tenTaiKhoan = tenTaiKhoan;
+    }
+
+    public String getMatKhau() {
+        return matKhau;
+    }
+
+    public void setMatKhau(String matKhau) {
+        this.matKhau = matKhau;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Integer getMaNhanVien() {
+        return maNhanVien;
+    }
+
+    public void setMaNhanVien(Integer maNhanVien) {
+        this.maNhanVien = maNhanVien;
+    }
+
+    public Integer getMaKH() {
+        return maKH;
+    }
+
+    public void setMaKH(Integer maKH) {
+        this.maKH = maKH;
+    }
+}
