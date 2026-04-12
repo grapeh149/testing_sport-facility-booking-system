e6a227ad0c15a42df1907bb01c25cf0235e8f37e
2251010012chau-png <2251010012chau@ou.edu.vn>
2026-04-12 21:38:57 +0700

    add NotificationRepository,TaiKhoanRepository, UserRepository

--- backend/src/main/java/group6/it/ou/sportfacilitybooking/repository/TaiKhoanRepository.java
+++ backend/src/main/java/group6/it/ou/sportfacilitybooking/repository/TaiKhoanRepository.java
@@ -3,10 +3,10 @@ package group6.it.ou.sportfacilitybooking.repository;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import group6.it.ou.sportfacilitybooking.entity.TaiKhoan;
+import group6.it.ou.sportfacilitybooking.entity.User;
 
 @Repository
-public interface TaiKhoanRepository extends JpaRepository<TaiKhoan, Integer> {
-    TaiKhoan findByTenTaiKhoan(String tenTaiKhoan);
-    List<TaiKhoan> findByRole(String role);
+public interface TaiKhoanRepository extends JpaRepository<User, Integer> {
+    User findByTenTaiKhoan(String tenTaiKhoan);
+    List<User> findByRole(String role);
 }
