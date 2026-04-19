6007d3381649e2c596e865cb8ef49f56fcc46b54
2251010012chau-png <2251010012chau@ou.edu.vn>
2026-04-19 21:04:40 +0700

    Add UpdatePasswordRequest

---
+++ backend/src/main/java/group6/it/ou/sportfacilitybooking/dto/UpdatePasswordRequest.java
@@ -0,0 +1,25 @@
+package group6.it.ou.sportfacilitybooking.request;
+
+public class UpdatePasswordRequest {
+    private String currentPassword;
+    private String newPassword;
+    private String confirmPassword;
+
+    public UpdatePasswordRequest() {}
+
+    public UpdatePasswordRequest(String currentPassword, String newPassword, String confirmPassword) {
+        this.currentPassword = currentPassword;
+        this.newPassword = newPassword;
+        this.confirmPassword = confirmPassword;
+    }
+
+    // Getters & Setters
+    public String getCurrentPassword() { return currentPassword; }
+    public void setCurrentPassword(String currentPassword) { this.currentPassword = currentPassword; }
+
+    public String getNewPassword() { return newPassword; }
+    public void setNewPassword(String newPassword) { this.newPassword = newPassword; }
+
+    public String getConfirmPassword() { return confirmPassword; }
+    public void setConfirmPassword(String confirmPassword) { this.confirmPassword = confirmPassword; }
+}
