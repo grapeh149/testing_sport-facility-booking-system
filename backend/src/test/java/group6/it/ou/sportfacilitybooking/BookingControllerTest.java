5699aa4aabadeee6516734738cb8983de1ca0024
grapeh149 <hthuy1409@gmail.com>
2026-05-03 22:24:42 +0700

    .

--- backend/src/test/java/group6/it/ou/sportfacilitybooking/BookingControllerTest.java
+++ backend/src/test/java/group6/it/ou/sportfacilitybooking/BookingControllerTest.java
@@ -12,7 +12,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
